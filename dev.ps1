1f34df15c65350784749f06d87024180630ba283
Paul Bevis <pbevis@hotmail.com>
2015-04-07 00:20:31 +0100

    Update dev.ps1
    Added 7zip and switched to using choco install instead of cinst.

--- dev.ps1
+++ dev.ps1
@@ -2,10 +2,11 @@ Write-Host "Installing Chocolatey"
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
 Write-Host "Installing applications from Chocolatey"
-cinst git -y
-cinst notepadplusplus -y
-cinst Firefox -y
-cinst GoogleChrome -y
-cinst fiddler4 -y
-cinst winmerge -y
-cinst windowsazurepowershell -y
+choco install 7zip -y
+choco install git -y
+choco install notepadplusplus -y
+choco install Firefox -y
+choco install GoogleChrome -y
+choco install fiddler4 -y
+choco install winmerge -y
+choco install windowsazurepowershell -y
