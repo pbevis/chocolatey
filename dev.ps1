4a80fb92830c77a2b0d91d1581bfb9c88a67a836
Paul Bevis <pbevis@hotmail.com>
2015-04-07 08:32:01 +0100

    Update dev.ps1
    Added Vagrant configuration.

--- dev.ps1
+++ dev.ps1
@@ -1,7 +1,7 @@
-Write-Host "Installing Chocolatey"
+Write-Host "Installing Chocolatey..."
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
-Write-Host "Installing applications from Chocolatey"
+Write-Host "Installing applications from Chocolatey..."
 choco install 7zip -y
 choco install git -y
 choco install notepadplusplus -y
@@ -10,3 +10,7 @@ choco install GoogleChrome -y
 choco install fiddler4 -y
 choco install winmerge -y
 choco install windowsazurepowershell -y
+choco install vagrant -y
+
+Write-Host "Installing vagrant plugins..."
+vagrant plugin install vagrant-azure
