b5f0232fb8051a46085c9b6f5c3807143246ef69
Paul Bevis <pbevis@hotmail.com>
2015-04-21 21:58:58 +0100

    Update Install-AD.ps1

--- Install-AD.ps1
+++ Install-AD.ps1
@@ -1,3 +1,5 @@
+Write-Host "Installing AD prerequisites..."
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 Import-Module ADDSDeployment
+Write-Host "Installing AD..."
 Install-ADDSForest -ForestMode "Win2012R2" -DomainName "dev.com" -DomainMode "Win2012R2" -DomainNetbiosName "DEV" -SafeModeAdministratorPassword (ConvertTo-SecureString "Password123" -AsPlainText -Force) -InstallDns -NoRebootOnCompletion -Force
